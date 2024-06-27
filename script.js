0871905b75ee96c99ad515477142e75c79402349
Subharoy78242 <57645302+Subharoy78242@users.noreply.github.com>
2024-06-27 12:21:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function getFormvalue() {
-    //Write your code here
+    // Prevent form from submitting
+    event.preventDefault();
 
+    // Get the values from the form inputs
+    const firstName = document.getElementsByName('fname')[0].value;
+    const lastName = document.getElementsByName('lname')[0].value;
+
+    // Display the values in an alert
+    alert(firstName + " " + lastName);
 }
